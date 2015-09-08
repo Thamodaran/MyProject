11f93490dc221f83aadc733762687e0bba568cdb
Thamodaran <kthamodaran@gmail.com>
2015-09-08 17:45:26 +0530

    Welcome page

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+	<head></head>
+<body>
+	<form method="POST" name="newform" action="val.php">
+		<label>First Name :</label><input type="text" name="fname">
+		<label>Second Name :</label><input type="text" name="sname">
+	</form>
+</body>
+</html>
