45e35f5f97f8bd45e10afe9cb552a45f777ee91f
Thamodaran <kthamodaran@gmail.com>
2015-09-08 17:59:27 +0530

    DIV added to welcome page

--- welcome.php
+++ welcome.php
@@ -1,6 +1,7 @@
 <html>
 	<head></head>
 <body>
+<div>
 	<form method="POST" name="newform" action="val.php">
 		<label>First Name :</label><input type="text" name="fname">
 		<label>Second Name :</label><input type="text" name="sname">
@@ -8,5 +9,6 @@
 		<label>Confirm Password :</label><input type="password" name="cpwd">
 		<button type="button">Sign up</button>
 	</form>
+</div>
 </body>
 </html>
