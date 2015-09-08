2fdbfdbe68cd62077197b1f2d25dc9f7033f8ba2
Thamodaran <kthamodaran@gmail.com>
2015-09-08 17:57:32 +0530

    Modified welcome page

--- welcome.php
+++ welcome.php
@@ -4,6 +4,9 @@
 	<form method="POST" name="newform" action="val.php">
 		<label>First Name :</label><input type="text" name="fname">
 		<label>Second Name :</label><input type="text" name="sname">
+		<label>Password :</label><input type="password" name="pwd">
+		<label>Confirm Password :</label><input type="password" name="cpwd">
+		<button type="button">Sign up</button>
 	</form>
 </body>
 </html>
