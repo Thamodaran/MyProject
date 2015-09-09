041c636e69ecf963e1e9847add058f16ffef7de5
Thamodaran <kthamodaran@gmail.com>
2015-09-09 17:45:35 +0530

    day1

---
+++ day1.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<link rel="stylesheet" href="mystyle.css">
+	<title>Day1 schdule</title>
+<body>
+	<nav>We are happy to reporting</nav>
+	<div>
+		<p>In day one we are introduction my self and introduction about the company (Ideas 2it) dy mr.Akilan</p>
+	</div>
+</body>
+</html>
