60bd900e844610943d7d2a3d29689f57226ac96a
Thamodaran <kthamodaran@gmail.com>
2015-09-09 18:03:36 +0530

    day2

---
+++ day2.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+        <link rel="stylesheet" href="mystyle.css">
+        <title>Day2 schdule</title>
+<body>
+        <nav>We are happy to reporting</nav>
+        <div>
+             <h3>Machine setup</h3>
+		<ol>
+		    <li>Appach instalation</li>
+       		    <li>MySQL instalation</li>
+		    <li>Php instalation</li>
+		</ol>
+	</div>
+</body>
+</html>
