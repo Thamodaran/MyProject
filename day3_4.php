ac3cd217887d7186e96314f6be05c55c65d79d92
Thamodaran <kthamodaran@gmail.com>
2015-09-09 18:04:25 +0530

    day3 to 4

---
+++ day3_4.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+        <link rel="stylesheet" href="mystyle.css">
+        <title>Day2 schdule</title>
+<body>
+        <nav>We are happy to reporting</nav>
+        <div>
+             <h3>Php basics</h3>
+		<h4>Day-3 Status</h4>
+		<ol>
+		    <li>Introduction of Php</li>
+       		    <li>Php syntax</li>
+		    <li>Php variables</li>
+		    <li>Output statements</li>
+		</ol>
+		<h4>Day-4 Status</h4>
+		<ol>
+		    <li>Php Datatypes</li>
+		    <li>Php string functions</li>
+		    <li>Constant </li>
+		    <li>Operator </li>
+		</ol>
+	</div>
+</body>
+</html>
