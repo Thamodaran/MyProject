47723b6cc8e24ca25ffb6a4cba8b9679a57e9a7e
Thamodaran <kthamodaran@gmail.com>
2015-09-09 18:05:26 +0530

    day5

---
+++ day5.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+        <link rel="stylesheet" href="mystyle.css">
+        <title>Day4 schdule</title>
+<body>
+        <nav>We are happy to reporting</nav>
+        <div>
+             <h3>Form UI</h3>
+                <ol>
+                    <li>Form handling</li>
+                    <li>Form Validation</li>
+                    <li>Required fields</li>
+                    <li>Email validation</li>
+                </ol>
+        </div>
+</body>
+</html>
+
